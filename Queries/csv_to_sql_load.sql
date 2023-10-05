0b626b005ff9ed26f8c04da6ec0a94f77ddeaf1a
Shreya <mailto.shreya.tarle@gmail.com>
2023-10-05 19:57:36 +0530

    s

--- Queries/csv_to_sql_load.sql
+++ Queries/csv_to_sql_load.sql
@@ -1,4 +1,4 @@
---QUERY FOR LOADING CSV TO SQL TABLE
+--QUERY FOR LOADING CSV TO TABLE
 COPY SHREYA.order_items_dataset(order_id,order_item_id,product_id,seller_id,shipping_limit_date,price,freight_value)
 FROM 'F:\LEARNINGS & TRAININGS\PostgreSQL\Dataset\olist_order_items_dataset.csv'
 DELIMITER ','
